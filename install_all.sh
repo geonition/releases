95a39f2c77dec23db5b822953587d922566da871
Mikko SA. Johansson <sa.msjohans@softgis.org.aalto.fi>
2012-04-26 14:14:05 +0300

    Updated dashboard version

--- install_all.sh
+++ install_all.sh
@@ -5,7 +5,7 @@ easy_install https://github.com/geonition/django_geojson_rest/tarball/4.0.0
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/4.0.0
-easy_install https://github.com/geonition/dashboard/tarball/4.0.0
+easy_install https://github.com/geonition/dashboard/tarball/4.0.2
 easy_install https://github.com/geonition/planproposal/tarball/4.0.0
 easy_install https://github.com/geonition/auth_page/tarball/4.0.0
 easy_install https://github.com/geonition/geoforms/tarball/4.0.0
