823c5f9180303599b598173b54b76e482c8ff3d7
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-05-22 12:33:42 +0300

    some modifcations to the data_migrate command and install_notes

--- geonition/manage_release/management/commands/data_migrate.py
+++ geonition/manage_release/management/commands/data_migrate.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.core.management import call_command
 import json
+import shutil
 
 class Command(BaseCommand):
     
@@ -11,20 +12,30 @@ class Command(BaseCommand):
             
             # load dumped data for apps if .json file found
             try:
-                jsonfile = open("%s%s.json" % (data_folder, app), 'rw')
+                shutil.copyfile( "%s%s.json" % (data_folder, app), "%s%s.json~" % (data_folder, app) )
+                jsonfile = open("%s%s.json~" % (data_folder, app), 'r')
+                new_jsonfile = open("%s%s.json" % (data_folder, app), 'w')
+
                 #modify the jsonfile if required for migrating
                 if app == 'geoforms':
                     json_obj = json.loads(jsonfile.read())
+                    new_json = []
                     for obj in json_obj:
                         try:
                             del obj['fields']['lang']
                         except KeyError:
                             pass
-                    json_file.write(json.dumps(json_obj))
-                
+                        new_json.append(obj)
+                    new_jsonfile.write(json.dumps(new_json))
+                    new_jsonfile.close()
+                    jsonfile.close()
+                    
                 #load the modified json file
-                call_command('loaddata', app, "%s%s.json" % (data_folder, app))
+                try:
+                    call_command('loaddata', app, "%s%s.json" % (data_folder, app))
+                except:
+                    pass
             
-            except:
+            except Exception:
                 pass
         
