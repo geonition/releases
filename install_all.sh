0750517ecef1536d81d74254eab79f95c3ea1f56
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-05-24 14:51:22 +0300

    version for geojson changed one up

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,6 @@
 easy_install https://github.com/geonition/django_geonition_utils/tarball/4.0.1
 easy_install https://github.com/geonition/django_auth/tarball/4.0.1
-easy_install https://github.com/geonition/django_geojson_rest/tarball/5.1.1
+easy_install https://github.com/geonition/django_geojson_rest/tarball/5.1.2
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/4.1.0
