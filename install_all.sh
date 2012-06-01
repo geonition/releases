4927def0e43e4c1844bbe842c5c2467e24be5604
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-06-01 13:30:11 +0300

    install_all dashboard and geoforms versions increased

--- install_all.sh
+++ install_all.sh
@@ -4,10 +4,10 @@ easy_install https://github.com/geonition/django_geojson_rest/tarball/5.2.0
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/4.2.3
-easy_install https://github.com/geonition/dashboard/tarball/5.0.0
+easy_install https://github.com/geonition/dashboard/tarball/5.0.1
 easy_install https://github.com/geonition/planproposal/tarball/4.0.1
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
-easy_install https://github.com/geonition/geoforms/tarball/4.1.4
+easy_install https://github.com/geonition/geoforms/tarball/4.1.5
 easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.0.0
 easy_install https://github.com/geonition/django_images/tarball/4.0.0
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
