c8b2cd3942c48197f8caacb90df6ca0403e32c9e
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-06-01 12:33:41 +0300

    modified everything to deal with the data migration for dashboard timestamp and the tooltip removal

--- data_migrate.py
+++ data_migrate.py
@@ -4,7 +4,6 @@ import settings
 setup_environ(settings)
 
 jsonfile = open("data/geojson_rest.json")
-print jsonfile
 
 import json
 
@@ -12,53 +11,23 @@ json_list = json.loads(jsonfile.read())
 
 jsonfile.close()
 
-data_dict = {}
+def remove_field(field_name, json_list):
+    print "removing field %s" % field_name
+    new_list = []
+    
+    for json_dict in json_list:
+        del json_dict['fields'][field_name]
+        new_list.append(json_dict)
+        
+    return new_list
 
-for obj in json_list:
-    if obj['model'] == 'geojson_rest.feature':
-        data_dict[obj['pk']] = {}
-        data_dict[obj['pk']]['feature'] = obj['fields']
-
-
-for obj in json_list:
-    if obj['model'] == 'geojson_rest.property':
-        data_dict[obj['pk']]['property'] = obj['fields']
-
-
-from geojson_rest.models import Feature
-from geojson_rest.models import Property
-from django.db import models
-from django.conf import settings
-from django.contrib.gis.db import models as gismodels
-from django.contrib.gis.gdal import OGRGeometry
-from django.contrib.auth.models import User
-from geonition_utils.models import JSON
-from geonition_utils.models import TimeD
-
-for key, value in data_dict.items():
-    print key
-    print value
-
-    #create feature
-    feature = value['feature']
-    geometry = OGRGeometry(feature['geometry']).geos
-    private = feature.get('private', True)
-    user = User.objects.get(id = feature['user'])
-    group = 'PP-perhela-perhelan-kortteli' #they are all put into the same group Jarvenpaa
-    time = TimeD(create_time = feature['create_time'],
-                 expire_time = feature['expire_time'])
-    time.save()
-    new_feature = Feature(geometry = geometry,
-                          user = user,
-                          group = group,
-                          private = private,
-                          time = time)
-    new_feature.save()
-
-    #create property
-    timed = TimeD()
-    timed.save()
-    time = timed
-    prop = Property()
-    #self.properties.add(prop)
+def add_field(field_name, default_value = None):
+    print "adding new field %s" % field_name
+    new_list = []
+    
+    for json_dict in json_list:
+        json_dict['fields'][field_name] = default_value
+        new_list.append(json_dict)
+        
+    return new_list
 
--- geonition/manage_release/management/commands/data_migrate.py
+++ geonition/manage_release/management/commands/data_migrate.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.core.management import call_command
+from django.core.serializers.base import DeserializationError
+import datetime
 import json
-import shutil
 
 class Command(BaseCommand):
+    help = "The command will migrate dumped data from previous version to current version of releases. Give as an agument the absolute path with a trailing backslash to the folder where the dumped data exists."
     
     def handle(self, *args, **options):
         data_folder = args[0]
@@ -12,20 +14,20 @@ class Command(BaseCommand):
             
             # load dumped data for apps if .json file found
             try:
-                shutil.copyfile( "%s%s.json" % (data_folder, app), "%s%s.json~" % (data_folder, app) )
-                jsonfile = open("%s%s.json~" % (data_folder, app), 'r')
-                new_jsonfile = open("%s%s.json" % (data_folder, app), 'w')
+                jsonfile = open("%s%s.json" % (data_folder, app), 'r')
+                new_jsonfile = open("%s%s_new.json" % (data_folder, app), 'w')
 
                 #modify the jsonfile if required for migrating
-                if app == 'geoforms':
-                    json_obj = json.loads(jsonfile.read())
-                    new_json = []
-                    for obj in json_obj:
-                        try:
-                            del obj['fields']['lang']
-                        except KeyError:
-                            pass
-                        new_json.append(obj)
+                if app == 'dashboard':
+                    new_json = []                    
+                    new_json = remove_field('tooltip', json.loads(jsonfile.read()))
+
+                    for lang in settings.LANGUAGES:
+                        lang_code = lang[0]
+                        field_name = "tooltip_%s" % lang_code
+                        new_json = remove_field(field_name, new_json)
+                        
+                    new_json = add_field("modify_date", new_json, default_value = datetime.datetime.now().strftime("%Y-%m-%d"))
                     new_jsonfile.write(json.dumps(new_json))
                 
                 else:
@@ -35,11 +37,32 @@ class Command(BaseCommand):
                 jsonfile.close()
                     
                 #load the modified json file
-                try:
-                    call_command('loaddata', app, "%s%s.json" % (data_folder, app))
-                except:
-                    pass
+                call_command('loaddata', app, "%s%s_new.json" % (data_folder, app))
             
-            except Exception:
+            except IOError:
                 pass
+            except DeserializationError:
+                print "Make sure you installed the current version of the applications using >>> sh install_all.sh command"
+   
+def remove_field(field_name, json_list):
+    print "removing field %s" % field_name
+    new_list = []
+    
+    for json_dict in json_list:
+        try:
+            del json_dict['fields'][field_name]
+        except KeyError:
+            pass
+        new_list.append(json_dict)
         
+    return new_list
+
+def add_field(field_name, json_list, default_value = None):
+    print "adding new field %s" % field_name
+    new_list = []
+    
+    for json_dict in json_list:
+        json_dict['fields'][field_name] = default_value
+        new_list.append(json_dict)
+        
+    return new_list     
