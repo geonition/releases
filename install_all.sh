55e95e671238eb40f8cb0a55d58cffac41de9b7a
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-06-14 14:46:29 +0300

    set geojson_rest as master version until tested for new version release

--- install_all.sh
+++ install_all.sh
@@ -1,6 +1,6 @@
 easy_install https://github.com/geonition/django_geonition_utils/tarball/4.0.2
 easy_install https://github.com/geonition/django_auth/tarball/4.0.1
-easy_install https://github.com/geonition/django_geojson_rest/tarball/5.2.0
+easy_install https://github.com/geonition/django_geojson_rest/tarball/master
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/4.2.3
