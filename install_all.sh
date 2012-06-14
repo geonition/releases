2a08994c3511c71ed246a9838e51603bf998e596
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-06-14 15:42:02 +0300

    install all updated and new versions tested

--- install_all.sh
+++ install_all.sh
@@ -1,13 +1,13 @@
 easy_install https://github.com/geonition/django_geonition_utils/tarball/4.0.2
 easy_install https://github.com/geonition/django_auth/tarball/4.0.1
-easy_install https://github.com/geonition/django_geojson_rest/tarball/master
+easy_install https://github.com/geonition/django_geojson_rest/tarball/5.2.1
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/4.2.3
 easy_install https://github.com/geonition/dashboard/tarball/5.0.1
 easy_install https://github.com/geonition/planproposal/tarball/4.0.1
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
-easy_install https://github.com/geonition/geoforms/tarball/4.1.6
+easy_install https://github.com/geonition/geoforms/tarball/4.1.7
 easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.0.0
 easy_install https://github.com/geonition/django_images/tarball/4.0.0
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
