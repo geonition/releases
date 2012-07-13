be5d1e84fdeb665ec0560c44dc5bfb23c6d60d74
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-07-13 11:15:00 +0300

    changes to the configuration files for uwsgi and wsgi.py

--- geonition/geonition/wsgi.py
+++ geonition/geonition/wsgi.py
@@ -15,7 +15,8 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geonition.settings_jarvenpaa")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geonition.settings_example")
+os.environ['PYTHON_EGG_CACHE'] = '/tmp/python_egg_cache'
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
