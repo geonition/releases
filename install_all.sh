bbb06480440d692e0ab82db6607f428b343a1d2d
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-07-17 11:05:30 +0300

    next versions for release

--- install_all.sh
+++ install_all.sh
@@ -3,12 +3,12 @@ easy_install https://github.com/geonition/django_auth/tarball/4.0.1
 easy_install https://github.com/geonition/django_geojson_rest/tarball/5.2.2
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-easy_install https://github.com/geonition/base_page/tarball/4.2.5 #tooltip css
+easy_install https://github.com/geonition/base_page/tarball/4.2.5
 easy_install https://github.com/geonition/dashboard/tarball/5.0.1
-easy_install https://github.com/geonition/planproposal/tarball/4.1.0 #many changes
+easy_install https://github.com/geonition/planproposal/tarball/4.1.1 #translations and buttons
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4 
-easy_install https://github.com/geonition/geoforms/tarball/4.1.8 #minor change with tooltip
-easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.1.0 # namespaces
+easy_install https://github.com/geonition/geoforms/tarball/4.1.8
+easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.1.0
 easy_install https://github.com/geonition/django_images/tarball/4.0.0
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install django-rosetta
