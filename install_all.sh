3651b63323230c85e70b755e121100b1587fb65f
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-07-17 11:49:18 +0300

    dashboard version set to master, should be updated next time to 5.0.2 or higher version

--- install_all.sh
+++ install_all.sh
@@ -4,7 +4,7 @@ easy_install https://github.com/geonition/django_geojson_rest/tarball/5.2.2
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/4.2.5
-easy_install https://github.com/geonition/dashboard/tarball/5.0.1
+easy_install https://github.com/geonition/dashboard/tarball/master
 easy_install https://github.com/geonition/planproposal/tarball/4.1.1 #translations and buttons
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4 
 easy_install https://github.com/geonition/geoforms/tarball/4.1.8
