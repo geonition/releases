3c41de0415ee47812f201e8340e9120233835ceb
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-07-18 15:11:26 +0300

    versions modified to master for next release

--- install_all.sh
+++ install_all.sh
@@ -3,9 +3,9 @@ easy_install https://github.com/geonition/django_auth/tarball/4.0.1
 easy_install https://github.com/geonition/django_geojson_rest/tarball/5.2.2
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-easy_install https://github.com/geonition/base_page/tarball/4.2.5
-easy_install https://github.com/geonition/dashboard/tarball/master
-easy_install https://github.com/geonition/planproposal/tarball/4.1.1 #translations and buttons
+easy_install https://github.com/geonition/base_page/tarball/master # styles from other apps override base_page styles and modernizr updated
+easy_install https://github.com/geonition/dashboard/tarball/master # Openlayers version to gnt
+easy_install https://github.com/geonition/planproposal/tarball/master # popups on highlight other layer features
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4 
 easy_install https://github.com/geonition/geoforms/tarball/4.1.8
 easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.1.0
