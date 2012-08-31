50d5f8250e3ec83795cd51b7911eeab9add2f3e8
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-08-31 11:48:58 +0300

    install all modified

--- install_all.sh
+++ install_all.sh
@@ -3,11 +3,11 @@ easy_install https://github.com/geonition/django_auth/tarball/4.0.1
 easy_install https://github.com/geonition/django_geojson_rest/tarball/5.3.0 #admin view geometry
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-easy_install https://github.com/geonition/base_page/tarball/master # responsive width on left
+easy_install https://github.com/geonition/base_page/tarball/master # responsive width on left, mobile version started
 easy_install https://github.com/geonition/dashboard/tarball/5.0.3
 easy_install https://github.com/geonition/planproposal/tarball/4.2.2
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
-easy_install https://github.com/geonition/geoforms/tarball/5.0.0 # admin improvements, DB element name + slug to 200 chars
+easy_install https://github.com/geonition/geoforms/tarball/5.0.0 # admin improvements, DB element name + slug to 200 chars + responsive content (bigcontent)
 easy_install https://github.com/geonition/geodjango-map-layers/tarball/master #initial map layers fixture added
 easy_install https://github.com/geonition/django_images/tarball/4.1.0 # svg images/icons added
 easy_install django-rosetta
