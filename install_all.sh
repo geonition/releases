30f6a3e9d4fe04ac57da73bb0f15ceb229554ce8
Mikko Johansson <mikko.johansson@aalto.fi>
2012-08-31 12:56:45 +0300

    another install_all update

--- install_all.sh
+++ install_all.sh
@@ -3,7 +3,7 @@ easy_install https://github.com/geonition/django_auth/tarball/4.0.1
 easy_install https://github.com/geonition/django_geojson_rest/tarball/5.3.0 #admin view geometry
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-easy_install https://github.com/geonition/base_page/tarball/master # responsive width on left, mobile version started
+easy_install https://github.com/geonition/base_page/tarball/4.2.9 # responsive width on left, mobile version started
 easy_install https://github.com/geonition/dashboard/tarball/5.0.3
 easy_install https://github.com/geonition/planproposal/tarball/4.2.2
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
