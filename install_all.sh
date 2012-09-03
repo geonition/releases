8b4cb2113a5e22e411e6b9685e7e253390b1e1e1
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-09-03 13:04:02 +0300

    set geoforms and base_page to master for update, and comments what was changed

--- install_all.sh
+++ install_all.sh
@@ -1,13 +1,13 @@
 easy_install https://github.com/geonition/django_geonition_utils/tarball/4.2.0
 easy_install https://github.com/geonition/django_auth/tarball/4.0.1
-easy_install https://github.com/geonition/django_geojson_rest/tarball/5.3.0 #admin view geometry
+easy_install https://github.com/geonition/django_geojson_rest/tarball/5.3.0 
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-easy_install https://github.com/geonition/base_page/tarball/4.2.9 # responsive width on left, mobile version started
+easy_install https://github.com/geonition/base_page/tarball/master # mobile version alpha, 
 easy_install https://github.com/geonition/dashboard/tarball/5.0.3
 easy_install https://github.com/geonition/planproposal/tarball/4.2.2
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
-easy_install https://github.com/geonition/geoforms/tarball/5.1.0 # UI fixes
-easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.1.2 #initial map layers fixture added
-easy_install https://github.com/geonition/django_images/tarball/4.1.0 # svg images/icons added
+easy_install https://github.com/geonition/geoforms/tarball/master #mobile version alpha, linear structured questionnaires
+easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.1.2
+easy_install https://github.com/geonition/django_images/tarball/4.1.0
 easy_install django-rosetta
