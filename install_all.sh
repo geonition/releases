18c15d258b66b6dcd9a8858adb1c49d5afb35560
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-09-03 13:13:58 +0300

    breadcrumbs fix in both plan proposals as in dashboard

--- install_all.sh
+++ install_all.sh
@@ -4,8 +4,8 @@ easy_install https://github.com/geonition/django_geojson_rest/tarball/5.3.0
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/master # mobile version alpha, 
-easy_install https://github.com/geonition/dashboard/tarball/5.0.3
-easy_install https://github.com/geonition/planproposal/tarball/4.2.2
+easy_install https://github.com/geonition/dashboard/tarball/master # breadcrumbs
+easy_install https://github.com/geonition/planproposal/tarball/master # breadcrumbs
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
 easy_install https://github.com/geonition/geoforms/tarball/master #mobile version alpha, linear structured questionnaires
 easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.1.2
