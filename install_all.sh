6940f9f263193b8dc410f2b609e85110465c6274
Mikko Johansson <mikko.johansson@aalto.fi>
2012-09-04 11:06:11 +0300

    Updated versions in install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -3,11 +3,13 @@ easy_install https://github.com/geonition/django_auth/tarball/4.0.1
 easy_install https://github.com/geonition/django_geojson_rest/tarball/5.3.0 
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-easy_install https://github.com/geonition/base_page/tarball/master # mobile version alpha, 
-easy_install https://github.com/geonition/dashboard/tarball/master # breadcrumbs, interaction
-easy_install https://github.com/geonition/planproposal/tarball/master # breadcrumbs
+easy_install https://github.com/geonition/base_page/tarball/4.3.0 # mobile version alpha, 
+easy_install https://github.com/geonition/dashboard/tarball/5.0.4 # breadcrumbs, interaction
+easy_install https://github.com/geonition/planproposal/tarball/4.2.3 # breadcrumbs
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
-easy_install https://github.com/geonition/geoforms/tarball/master #mobile version alpha, linear structured questionnaires
+easy_install https://github.com/geonition/geoforms/tarball/5.2.0 #mobile version alpha, linear structured questionnaires
 easy_install https://github.com/geonition/geodjango-map-layers/tarball/4.1.2
 easy_install https://github.com/geonition/django_images/tarball/4.1.0
 easy_install django-rosetta
+# If needed install new version of lxml
+# CFLAGS=-fPIC STATICBUILD=true LIBXML2_VERSION=2.8.0 pip install lxml
