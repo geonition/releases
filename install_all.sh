8940e49913fd413b68368d479b0fcc5d359921a3
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-09-04 09:27:54 +0300

    comment added for dashboard

--- install_all.sh
+++ install_all.sh
@@ -4,7 +4,7 @@ easy_install https://github.com/geonition/django_geojson_rest/tarball/5.3.0
 easy_install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 easy_install https://github.com/geonition/django_geonition_client/tarball/4.0.0
 easy_install https://github.com/geonition/base_page/tarball/master # mobile version alpha, 
-easy_install https://github.com/geonition/dashboard/tarball/master # breadcrumbs
+easy_install https://github.com/geonition/dashboard/tarball/master # breadcrumbs, interaction
 easy_install https://github.com/geonition/planproposal/tarball/master # breadcrumbs
 easy_install https://github.com/geonition/auth_page/tarball/4.0.4
 easy_install https://github.com/geonition/geoforms/tarball/master #mobile version alpha, linear structured questionnaires
