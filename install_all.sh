349913a782f3d8e39b1e0a737bcbac098a6fccec
Mikko Johansson <mikko.johansson@aalto.fi>
2012-09-14 16:42:55 +0300

    There are problems with modeltranslation autodiscover(), so changed revision to before it

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,5 @@
 # Installs modeltranslation from svn repository to prevent XMLField error in test
-pip install svn+http://django-modeltranslation.googlecode.com/svn/trunk/@156
+pip install svn+http://django-modeltranslation.googlecode.com/svn/trunk/@139
 pip install https://github.com/geonition/django_geonition_utils/tarball/4.2.0
 pip install https://github.com/geonition/django_auth/tarball/4.0.1
 pip install https://github.com/geonition/django_geojson_rest/tarball/5.3.1
