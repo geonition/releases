257cb0398eddfc16cd0cfc4144a4814de8ac1b1d
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-09-20 12:18:56 +0300

    comments added to install_all

--- install_all.sh
+++ install_all.sh
@@ -5,11 +5,11 @@ pip install https://github.com/geonition/django_auth/tarball/master #Fixed tests
 pip install https://github.com/geonition/django_geojson_rest/tarball/master # pointfeature, polygonfeature, linestringfeature views added (They break the tests)
 pip install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 pip install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-pip install https://github.com/geonition/base_page/tarball/master #city name changed to organization
+pip install https://github.com/geonition/base_page/tarball/master #city name changed to organization, color inputs used in admin for organization colors
 pip install https://github.com/geonition/dashboard/tarball/master #city name changed to organization
 pip install https://github.com/geonition/planproposal/tarball/master #city name changed to organization
 pip install https://github.com/geonition/auth_page/tarball/4.0.4
-pip install https://github.com/geonition/geoforms/tarball/master #html5 widgets moved to geonition_utils
+pip install https://github.com/geonition/geoforms/tarball/master #html5 widgets moved to geonition_utils, hash changed to page_<nr>
 pip install https://github.com/geonition/geodjango-map-layers/tarball/4.1.2
 pip install https://github.com/geonition/django_images/tarball/4.1.0
 pip install django-rosetta
