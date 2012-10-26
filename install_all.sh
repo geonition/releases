d725277ca1dfead3dbf4d388a28b0e92a24049dd
Mikko Johansson <mikko.johansson@aalto.fi>
2012-10-26 12:27:37 +0300

    Updated versions in install_all

--- install_all.sh
+++ install_all.sh
@@ -5,7 +5,7 @@ pip install https://github.com/geonition/django_auth/tarball/4.1.0
 pip install https://github.com/geonition/django_geojson_rest/tarball/5.5.0 # all view models uses sama m2m table. m2m field views added to views 
 pip install https://github.com/geonition/django_opensocial_people/tarball/4.0.1
 pip install https://github.com/geonition/django_geonition_client/tarball/4.0.0
-pip install https://github.com/geonition/base_page/tarball/4.6.0 # updated JQuery, JQuery ui and mozernizer. Styles are in index.html. setup changes for OpenLayers.
+pip install https://github.com/geonition/base_page/tarball/4.6.1 # updated JQuery, JQuery ui and mozernizer. Styles are in index.html. setup changes for OpenLayers.
 pip install https://github.com/geonition/dashboard/tarball/5.2.0 
 pip install https://github.com/geonition/planproposal/tarball/4.3.0 
 pip install https://github.com/geonition/auth_page/tarball/4.0.4
